978beb273546388d84f20247ec0ae9ace6c7acd7
naoto0822 <n.h.in.m.h@gmail.com>
2017-06-25 16:34:19 +0900

    fix require lib

--- lib/slack-incoming-webhooks-ruby.rb
+++ lib/slack-incoming-webhooks-ruby.rb
@@ -1,4 +1,15 @@
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'json'
+
 require 'slack/version'
-require 'slack/incoming_webhooks'
-require 'slack/messages'
-require 'slack/http'
+require 'slack/incoming_webhooks/client'
+
+Dir[File.dirname(__FILE__) + '/slack/messages/**/*.rb'].each do |file|
+  require file
+end
+
+Dir[File.dirname(__FILE__) + '/slack/http/**/*.rb'].each do |file|
+  require file
+end
