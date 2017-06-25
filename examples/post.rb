aae94c1a335add64dcec0b6194d3dbe29101cd4e
naoto0822 <n.h.in.m.h@gmail.com>
2017-06-25 18:46:16 +0900

    fix sample

--- examples/post.rb
+++ examples/post.rb
@@ -3,12 +3,22 @@ require 'slack-incoming-webhooks-ruby'
 url = ENV['SLACK_WEBHOOK_URL']
 slack = Slack::IncomingWebhooks::Client.new(url)
 payload = Slack::Messages::Payload.new
-payload.text = 'test text'
+# payload.text = 'sample text'
 attachment = Slack::Messages::Attachment.new
-attachment.fallback = 'fallback sample'
+attachment.fallback = 'sample fallback'
 attachment.color = 'good'
-attachment.title = 'test post'
+attachment.title = 'sample title'
 attachment.footer = 'from sample'
+
+field = Slack::Messages::Field.new
+field.title = "sample field"
+field.value = 'http://github.com'
+
+field2 = Slack::Messages::Field.new
+field2.title = "sample field2"
+field2.value = 'http://github2.com'
+
+attachment.fields = [field, field2]
 payload.attachments = [attachment]
 
 slack.post(payload)
